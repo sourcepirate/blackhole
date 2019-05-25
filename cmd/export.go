41d3a69b242b1ed995cff2a61821ddc118c2d3da
sourcepirate <sathyanarrayanan@yandex.com>
2019-05-25 11:25:20 +0800

    updated export command

--- cmd/export.go
+++ cmd/export.go
@@ -1,15 +1,41 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/sourcepirate/blackhole/lib"
 	"github.com/spf13/cobra"
 )
 
 func exportCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:   "export",
+		Use:   "export [dumpfile] [url] [indexname]",
 		Short: "Export elasticsearch json to index",
 		Long:  "Export elasticsearch json to index",
+		Args:  cobra.MinimumNArgs(3),
 		Run: func(cmd *cobra.Command, args []string) {
+			arg := args[0]
+			url := args[1]
+			index := args[2]
+
+			fp, err := os.Open(arg)
+			if err != nil {
+				log.Println("Unable to open file %s", arg)
+			}
+
+			reader := bufio.NewReader(fp)
+			decoder := json.NewDecoder(reader)
+
+			exporter, errored := lib.NewExporter(url, batchsize)
+
+			if errored != nil {
+				log.Fatal("Errored durring export")
+			}
+
+			exporter.Export(index, decoder)
 
 		},
 	}
---
+++ lib/lib.go
@@ -0,0 +1,161 @@
+package lib
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+
+	"github.com/olivere/elastic"
+)
+
+func getClient(url string) (*elastic.Client, error) {
+	return elastic.NewSimpleClient(elastic.SetURL(url))
+}
+
+func getQuery(client *elastic.Client, index string, from int, size int) *elastic.SearchService {
+	query := elastic.NewMatchAllQuery()
+	return client.Search().Index(index).Query(query).Size(size).From(from)
+}
+
+// Dumper ...
+type Dumper struct {
+	client    *elastic.Client
+	patchsize int
+}
+
+// Dump ...
+func (d *Dumper) Dump(index string, encoder *json.Encoder) (bool, error) {
+	var start int
+	start = 0
+
+	mapping_service := elastic.NewGetMappingService(d.client)
+	mapping_service.Index(index)
+	meta_maps, map_err := mapping_service.Do(context.Background())
+
+	if map_err != nil {
+		// mapping error
+		log.Fatalf("Unable to fetch mapping for : %s", index)
+	}
+
+	map_encode_error := encoder.Encode(meta_maps)
+
+	if map_encode_error != nil {
+		// mapping encode error
+		log.Fatalf("Mapping encode error!!")
+	}
+
+	for {
+		// loops until no data available.
+		log.Printf("Querying %s from %d", index, start)
+		searchSerivce := getQuery(d.client, index, start, d.patchsize)
+		result, errored := searchSerivce.Do(context.Background())
+		if errored != nil {
+			return false, errors.New("ES Fetch Error")
+		}
+		start = start + len(result.Hits.Hits)
+		log.Printf("Fetched record %d - %d", len(result.Hits.Hits), start)
+
+		for _, it := range result.Hits.Hits {
+			data := make(map[string]interface{})
+			data["_id"] = it.Id
+			data["_source"] = it.Source
+			data["_doc"] = it.Type
+			errored = encoder.Encode(data)
+			if errored != nil {
+				return false, errors.New("File Writter error")
+			}
+		}
+
+		if int64(start) >= result.TotalHits() {
+			break
+		}
+	}
+
+	return true, nil
+}
+
+// NewDumper ...
+func NewDumper(url string, patchsize int) (*Dumper, error) {
+	client, err := getClient(url)
+	if err != nil {
+		return nil, err
+	}
+	return &Dumper{client, patchsize}, nil
+}
+
+type Data struct {
+	ID     string                 `json:"_id"`
+	Doc    string                 `json:"_doc"`
+	Source map[string]interface{} `json:"_source"`
+}
+
+// Exporter
+type Exporter struct {
+	client    *elastic.Client
+	patchsize int
+}
+
+func (d *Exporter) Export(index string, decoder *json.Decoder) {
+
+	var data Data
+
+	map_data := make(map[string]interface{})
+	mapping_err := decoder.Decode(&map_data)
+
+	if mapping_err != nil {
+		log.Fatalf("Decode Error for mapping")
+	}
+
+	index_service := elastic.NewIndexService(d.client)
+	index_service.BodyJson(map_data)
+	index_service.Index(index)
+
+	err := decoder.Decode(&data)
+
+	if err != nil {
+		log.Println("Errored")
+		log.Fatal(err)
+	}
+
+	index_service.Type(data.Doc)
+
+	_, creation_error := index_service.Do(context.Background())
+	// Error created.
+	if creation_error != nil {
+		log.Println(creation_error)
+		log.Fatalf("Failed creating index %s", index)
+	}
+
+	for decoder.More() {
+
+		if err != nil {
+			log.Println(err)
+
+			log.Fatal("Unable to decode data")
+		}
+		request := elastic.NewBulkIndexRequest()
+		request.Index(index)
+		request.Doc(data.Source)
+		request.Id(data.ID)
+
+		mapper := elastic.NewBulkService(d.client)
+		mapper.Index(index).Type(data.Doc).Add(request)
+
+		response, errored := mapper.Do(context.Background())
+		if errored != nil {
+			log.Panic(errored)
+			log.Fatal(response)
+		}
+		log.Println(response)
+		err = decoder.Decode(&data)
+	}
+}
+
+func NewExporter(url string, patchsize int) (*Exporter, error) {
+	client, err := getClient(url)
+	if err != nil {
+		return nil, err
+	}
+	return &Exporter{client, patchsize}, nil
+}
